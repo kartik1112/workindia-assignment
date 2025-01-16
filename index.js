17234a5c42a25850c3a7ed7eb496cf4e2e4d7702
Kartik Buttan <67007907+kartik1112@users.noreply.github.com>
2025-01-16 05:57:45 +0000

    temp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import db from "./initializers/db.js";
-import app from "./initializers/express-app.js"
+import app from "./routes/routes.js"
 
 // Default Port 3000 unless specified in .env
 const PORT = process.env.PORT || 3000;
